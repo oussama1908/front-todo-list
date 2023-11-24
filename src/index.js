b7c0cc3f2bda68c028f559533bcec50e170a6e92
oussama1908 <139545455+oussama1908@users.noreply.github.com>
2023-11-24 09:43:43 +0100

    Update index.js
    correction of the axios

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store'; 
 import axios from 'axios';
 // axios.defaults.baseURL="https://todo-b1t7.onrender.com/api/"
-axios.defaults.baseURL="https://todomernstack.onrender.com/api"
+axios.defaults.baseURL="https://todomernstack.onrender.com/api/"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
